e0a718ba1c21eddeeb9096c9aa4c29d97ff73887
Ali Ufuktan Topcu <114070829+AliTopcuu@users.noreply.github.com>
2026-02-26 10:22:07 +0000

    cors hataları için düzenleme

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ app = FastAPI(
     version="2.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Bu satırı "*" yaparak tüm dünyadan gelen isteklere kapıyı açıyoruz
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
 app.include_router(products_router)
 app.include_router(users_router)
